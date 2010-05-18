da99ea86013c1a04cdceee8c6b7d94d8bd2d5a38
Barry Carr <barry@barrycarr.org>
2010-05-18 21:49:50 +0100

    Added imports for HttpClient and BasicAuth

--- src/tweetmonitor/core.clj
+++ src/tweetmonitor/core.clj
@@ -1,4 +1,26 @@
-(ns tweetmonitor.core)
+(ns tweetmonitor.core
+ (:import (org.apache.http.impl.client DefaultHttpClient)
+          (org.apache.http.client.methods HttpGet))
+
+    (:import (org.apache.http HttpEntity)
+             (org.apache.http HttpException)
+             (org.apache.http HttpHost)
+    (org.apache.http HttpRequest)
+    (org.apache.http HttpRequestInterceptor)
+    ( org.apache.http HttpResponse)
+    ( org.apache.http.auth AuthScheme)
+    ( org.apache.http.auth AuthScope)
+    ( org.apache.http.auth AuthState)
+    ( org.apache.http.auth Credentials)
+    ( org.apache.http.auth UsernamePasswordCredentials)
+    ( org.apache.http.client CredentialsProvider)
+    ( org.apache.http.client.protocol ClientContext)
+    ( org.apache.http.impl.auth BasicScheme)
+    ( org.apache.http.protocol BasicHttpContext)
+    ( org.apache.http.protocol ExecutionContext)
+    ( org.apache.http.protocol HttpContext)))
 
 "http://stream.twitter.com/1/statuses/firehose.json"
 
+(.. (.execute (DefaultHttpClient. ) (HttpGet. "http://www.google.com"))
+    getEntity getContent)
